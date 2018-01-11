ac45243d1846881a8d6ed0f55b1102d407c492e9
filip2600 <filip2600@gmail.com>
2018-01-11 20:48:28 +0100

    Add files via upload

---
+++ Glowna.java
@@ -0,0 +1,16 @@
+
+public class Glowna {
+
+	public static void main(String[] args) {
+		
+		//1.Interfejs
+		Interfejs interfejs=new Interfejs();
+		
+		
+		
+		//2.Logowanie  po zalogowaniu przechodzi do wyszukiwarki produktow i pojawia sie mozliwosc wyloguj ktora wraca do poczatku
+		
+		//3.Rejestracja -Po rejestracji powraca do poczatkowego interfejsu
+	}
+
+}
---
+++ Interfejs.java
@@ -0,0 +1,62 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Image;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+
+public class Interfejs {
+
+	Interfejs()
+	{
+		JFrame ramkainterfejs=new JFrame();
+		ramkainterfejs.setMinimumSize(new Dimension(400, 350));
+		JPanel pramkainterfejs=new JPanel(new GridLayout());
+		pramkainterfejs.setBackground(Color.DARK_GRAY);
+		JPanel napisgora=new JPanel(new BorderLayout());
+	   Border margines=BorderFactory.createEmptyBorder(0,150,0,0);
+		JLabel powitanie=new JLabel("Hello");
+		powitanie.setBorder(margines);
+		powitanie.setFont(new Font("TimesRoman", Font.PLAIN, 30));
+		powitanie.setForeground(Color.GREEN);
+		JButton zaloguj=new JButton("zaloguj");
+		JButton zarejestruj=new JButton("Zarejestruj");
+		ImageIcon ikonkaobrazek = new ImageIcon("C:/ProjektyJava/TesterAlergii/obrazki/kciuk.jpg");
+		Image obrazeki=ikonkaobrazek.getImage();
+		Image nowyobrazek=obrazeki.getScaledInstance(250, 250, Image.SCALE_SMOOTH);
+		ImageIcon gotowy=new ImageIcon(nowyobrazek);
+		JLabel obrazek=new JLabel(gotowy);
+		
+		
+		
+		
+		
+		
+		
+		napisgora.add(powitanie);
+		pramkainterfejs.add(zaloguj);
+		pramkainterfejs.add(zarejestruj);
+		
+		
+		ramkainterfejs.add(napisgora,BorderLayout.NORTH);
+		ramkainterfejs.add(pramkainterfejs,BorderLayout.CENTER);
+		ramkainterfejs.add(obrazek,BorderLayout.SOUTH);
+		
+		
+		
+	
+		ramkainterfejs.setVisible(true);
+	}
+	
+	
+	
+	
+}
